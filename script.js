fc0ad8ccc7fdafa5928812503b9037b8ed42d62f
Heewon Ahn <57425238+shelost@users.noreply.github.com>
2022-02-14 22:19:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,32 @@ let NAV = document.getElementById('nav')
 let BUTTON = document.getElementById('send')
 let TEXTAREA = document.getElementById('message')
 let LINK = document.getElementById('link')
+let MENU = document.getElementById('menu')
+
+let NAVBTNS = document.getElementsByClassName('navbtn')
 
 TEXTAREA.oninput = () => {
     LINK.href = 'mailto:shelost.off@gmail.com?cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here'
     console.log('yo')
 }
+
+MENU.onclick = () => NAV.classList.add('active')
+
+
+for (let i=0; i<NAVBTNS.length; i++){
+    let navbtn = NAVBTNS[i]
+
+    navbtn.onclick = () => {
+        NAV.classList.remove('active')
+    }
+}
+
+let loop = () => {
+
+    if (window.innerWidth<800){
+
+    }
+
+    window.requestAnimationFrame(loop)
+}
+window.requestAnimationFrame(loop)
